153c745bb0988d1185176ba21b3494c840fb0efb
scottfarajigh <habibfaraji2002@gmail.com>
2023-04-10 02:44:12 -0700

    app_is_scrollable

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -24,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button saveInformationBtn = findViewById(R.id.main_saveInformation_btn);
+        saveInformationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Informations Saved " , Toast.LENGTH_LONG).show();
+            }
+        });
+
+        CheckBox androidCb = findViewById(R.id.main_android_cb);
+        androidCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(MainActivity.this,"Android Skill selected",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        CheckBox seepLearningCb = findViewById(R.id.main_deepLearning_cb);
+        seepLearningCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(MainActivity.this,"DeepLearning Skill selected",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        CheckBox uiCb = findViewById(R.id.main_ui_cb);
+        uiCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Toast.makeText(MainActivity.this,"UI/UX Skill selected",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
